0eb8886e28355ad719cf05bffc8e79843c2e50af
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-10 23:58:15 +0900

    Title: 수열 위에서의 이동, Time: 128ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240310/수열 위에서의 이동/movement-on-the-sequence.py
+++ 240310/수열 위에서의 이동/movement-on-the-sequence.py
@@ -3,10 +3,21 @@ li=list(map(int,input().split()))
 cnt=0
 cur=0
 while cur<=n-1:
-    temp=li[cur]
-    cnt+=1
-    if temp == 0:
+    # print(cur)
+    tempmax=li[cur]
+    # print(tempmax)
+    if cur+tempmax>n-1:
+        cnt+=1
+        break
+    if tempmax==1:
+        cnt+=1
+        cur+=1
+        continue
+    elif tempmax == 0:
         cnt=-1
         break
-    cur+=temp
+    else:
+        cnt+=1
+        l=li[cur+1:cur+tempmax+1]
+        cur+=(l.index(max(l))+1)
 print(cnt)
