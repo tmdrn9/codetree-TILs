0facfa25ce5f03f21634b339f3d4ba1978b9c782
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-16 19:00:09 +0900

    Title: 두 수의 합, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240316/두 수의 합/sum-of-two-num.py
+++ 240316/두 수의 합/sum-of-two-num.py
@@ -3,8 +3,10 @@ n,k=map(int,input().split())
 li=list(map(int,input().split()))
 
 cnt=0
-for i in li:
-    if k-i in li:
+for i,a in enumerate(li):
+    temp=li.pop(i)
+    if k-a in li:
         cnt+=1
+    li.append(temp)
 
 print(int(cnt/2))
