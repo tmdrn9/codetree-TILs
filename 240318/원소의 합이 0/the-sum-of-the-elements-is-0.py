7a99802e84f79e5e2e2dac42a50dab7d776a8aa1
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-18 18:16:55 +0900

    Title: 원소의 합이 0, Time: 5073ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

--- 240318/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240318/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -4,19 +4,31 @@ for i in range(4):
     li.append(list(map(int, input().split())))
 
 d1=dict()
-for j in list(set(li[0])):
-    d1[j]=1
+for j in li[0]:
+    if j in d1:
+        d1[j]+=1
+    else:
+        d1[j]=1
 
 d2=dict()
-for j in list(set(li[1])):
-    d2[j]=1
+for j in li[1]:
+    if j in d2:
+        d2[j]+=1
+    else:
+        d2[j]=1
 
 d3=dict()
-for j in list(set(li[2])):
-    d3[j]=1
+for j in li[2]:
+    if j in d3:
+        d3[j]+=1
+    else:
+        d3[j]=1
 d4=dict()
-for j in list(set(li[3])):
-    d4[j]=1
+for j in li[3]:
+    if j in d4:
+        d4[j]+=1
+    else:
+        d4[j]=1
 
 answer=0
 for i in d1:
@@ -24,5 +36,6 @@ for i in d1:
         for k in d3:
             s=i+j+k
             if -s in d4:
-                answer+=1
+                
+                answer+=(d1[i]*d2[j]*d3[k]*d4[-s])
 print(answer)
