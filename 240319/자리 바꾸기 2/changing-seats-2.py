7f95150991cc21af9cc291c85a027081f4e10b5d
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-19 22:18:36 +0900

    Title: 자리 바꾸기 2, Time: 5055ms, Memory: 112MB, Status: Time Limit Exceed - CodeTree

--- 240319/자리 바꾸기 2/changing-seats-2.py
+++ 240319/자리 바꾸기 2/changing-seats-2.py
@@ -3,8 +3,9 @@ n,k=map(int,input().split())
 li=[]
 d=dict()
 person=list(range(1,n+1))
+
 for i in range(n):
-    d[i]={person[i]}
+    d[i+1]={person[i]}
 for i in range(k):
     li.append(list(map(int,input().split())))
 
@@ -15,11 +16,9 @@ for i in range(3*k):
     person[change[1]-1]=temp
     if person==list(range(1,n+1)):
         break
-    d[change[0]-1].add(person[change[0]-1])
-    d[change[1]-1].add(person[change[1]-1])
+    d[person[change[0]-1]].add(change[0])
+    d[person[change[1]-1]].add(change[1])
+    # print(d)
 
-answer=[]
 for i in d.values():
-    answer.extend(list(i))
-for i in range(n):
-    print(answer.count(i+1))
+    print(len(i))
