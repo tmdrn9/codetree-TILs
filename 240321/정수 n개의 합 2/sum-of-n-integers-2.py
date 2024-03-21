402dbb13b436f662c3b5e5d3bc420d8ab5c624ee
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-21 18:08:42 +0900

    Title: 정수 n개의 합 2, Time: 142ms, Memory: 32MB, Status: Passed - CodeTree

--- 240321/정수 n개의 합 2/sum-of-n-integers-2.py
+++ 240321/정수 n개의 합 2/sum-of-n-integers-2.py
@@ -5,7 +5,7 @@ pixed_sum[0]=li[0]
 m=0
 for i in range(n-1):
     pixed_sum[i]=pixed_sum[i-1]+li[i]
-for i in range(k-1,n-k+1):
+for i in range(k-1,n):
     if i-k+1==0:
         if pixed_sum[i]>m:
             m=pixed_sum[i]
