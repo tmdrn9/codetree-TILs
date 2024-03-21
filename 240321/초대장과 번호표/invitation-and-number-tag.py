07ba9b933c9221c91d889f6aa2506e7df4f0289a
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-21 17:40:30 +0900

    Title: 초대장과 번호표, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240321/초대장과 번호표/invitation-and-number-tag.py
+++ 240321/초대장과 번호표/invitation-and-number-tag.py
@@ -12,7 +12,7 @@ total=[]
 total=[set(list(map(int,input().split()))[1:]) for _ in range(g)]
 
 i=0
-while True:
+while len(total) == i:
     gr=total[i]
     cha=gr-invite
     if len(cha) == 1:
@@ -22,8 +22,5 @@ while True:
         i=0
     else:
         i+=1
-
-    if len(total) == i :
-        break
 
 print(len(invite))
