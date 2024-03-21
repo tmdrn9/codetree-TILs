e82b533d7a0c928b635aa7656cb80f0cd3ede1c3
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-21 17:46:57 +0900

    Title: 초대장과 번호표, Time: 480ms, Memory: 37MB, Status: Passed - CodeTree

--- 240321/초대장과 번호표/invitation-and-number-tag.py
+++ 240321/초대장과 번호표/invitation-and-number-tag.py
@@ -1,16 +1,7 @@
 n,g=map(int,input().split())
 invite={1}
-total=[]
-# for _ in range(g):
-#     group=set(list(map(int,input().split()))[1:])
-#     total.append(group)
-#     if len(group-invite) == 1:
-#         for i in group:
-#             invite.add(i)
-
-
 total=[set(list(map(int,input().split()))[1:]) for _ in range(g)]
-
+total=sorted(total,key=lambda x:len(x))
 i=0
 while len(total) != i:
     gr=total[i]
