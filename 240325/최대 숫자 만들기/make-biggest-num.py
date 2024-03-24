221106f914ae3bf000a88e9ca546bdae31260c57
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-25 00:51:11 +0900

    Title: 최대 숫자 만들기, Time: 504ms, Memory: 28MB, Status: Passed - CodeTree

--- 240325/최대 숫자 만들기/make-biggest-num.py
+++ 240325/최대 숫자 만들기/make-biggest-num.py
@@ -1,11 +1,16 @@
+from functools import cmp_to_key
+# 이 순서가 우리가 원하는 순서라면 0보다 작은 값을, 
+# 반대라면 0보다 큰 값을
+# 둘의 우선순위가 동일하다면 0을 반환하면 됩니다.
+def compare(x,y):
+    if x+y>y+x:
+        return -1
+    if y+x>x+y:
+        return 1
+    return 0
+
 n=int(input())
 li=[input() for _ in range(n)]
-d=dict()
-answer=''
-for i in li:
-    t=int(10/len(i))
-    d[i*t]=i
-for i in sorted(d.items(),reverse=True):
-    answer+=i[1]
-print(answer)
-# print(''.join(answer))
+li=sorted(li,key=cmp_to_key(compare))
+
+print(''.join(li))
