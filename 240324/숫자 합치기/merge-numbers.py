8c6f949461ed3c7a8e5544c5f12941c32448e56b
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-24 23:31:34 +0900

    Title: 숫자 합치기, Time: 5073ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240324/숫자 합치기/merge-numbers.py
+++ 240324/숫자 합치기/merge-numbers.py
@@ -3,9 +3,9 @@ li=list(map(int,input().split()))
 answer=0
 while len(li)>2:
     li=sorted(li)
-    temp=li[0]+li[1]
-    li.remove(li[0])
-    li.remove(li[1])
+    a,b=li[0],li[1]
+    temp=a+b
+    li=li[2:]
     answer+=temp
     li.append(temp)
 print(answer+sum(li))
