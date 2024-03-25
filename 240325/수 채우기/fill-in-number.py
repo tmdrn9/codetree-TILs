94592344a4fd2da07bab2a7d801eefe214a70a75
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-25 19:00:42 +0900

    Title: 수 채우기, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240325/수 채우기/fill-in-number.py
+++ 240325/수 채우기/fill-in-number.py
@@ -2,14 +2,15 @@ n=int(input())
 n5=0
 if n<5 and n%2!=0:
     print(-1)
-n5,remain=divmod(n,5)
-if remain==0:
-    print(n5)
-elif remain%2==0:
-    n2,_=divmod(remain,2)
-    print(n5+n2)
 else:
-    if remain==1:
-        print(n5+2)
+    n5,remain=divmod(n,5)
+    if remain==0:
+        print(n5)
+    elif remain%2==0:
+        n2,_=divmod(remain,2)
+        print(n5+n2)
     else:
-        print(n5+3)
+        if remain==1:
+            print(n5+2)
+        else:
+            print(n5+3)
