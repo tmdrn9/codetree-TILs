ec5e52b5df894b581adc46d3d22f413ab6d543a0
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-25 18:25:08 +0900

    Title: 수 채우기, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240325/수 채우기/fill-in-number.py
+++ 240325/수 채우기/fill-in-number.py
@@ -1,5 +1,8 @@
-n=int(input())
+n=3301 #int(input())
 n5,remain=divmod(n,5)
-n2,remain2=divmod(remain,2)
-answer= n5+n2 if remain2==0 else -1
-print(answer)
+if remain!=0:
+    n2,remain2=divmod(remain,2)
+    answer= n5+n2 if remain2==0 else -1
+    print(answer)
+else:
+    print(n5)
