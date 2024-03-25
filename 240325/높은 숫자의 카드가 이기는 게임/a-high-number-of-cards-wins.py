6a128f059a10a8ed94c2162c5d93e4644ef8eab6
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-25 20:30:10 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240325/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240325/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -1,7 +1,7 @@
 import heapq
 
 n=int(input())
-total={i for i in range(1,2*n)}
+total={i for i in range(1,(2*n)+1)}
 answer=0
 
 b=[int(input()) for _ in range(n)]
@@ -11,10 +11,13 @@ heapq.heapify(b)
 heapq.heapify(a)
 
 temp=heapq.heappop(b)
-while len(a)>0:
+while True:
     aa=heapq.heappop(a)
     if aa>temp:
         answer+=1
-        temp=heapq.heappop(b)
+        if len(b)>0:
+            temp=heapq.heappop(b)
+        else:
+            break
 
 print(answer)
