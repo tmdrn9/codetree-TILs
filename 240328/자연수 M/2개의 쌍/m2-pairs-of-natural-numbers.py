de4e5b82240dfe334083e2f3d5ba793d860590ac
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-28 00:29:23 +0900

    Title: 자연수 M/2개의 쌍, Time: 422ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

--- 240328/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 240328/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -1,14 +1,12 @@
 n=int(input())
 li=[]
-total=0
 for _ in range(n):
     x,y=map(int,input().split())
     li.extend([y]*x)
-    total+=x
 li=sorted(li)
 MIN=0
-for i in range(int(total/2)):
-    temp=li[i]+li[total-1-i]
+for i in range(int(len(li)/2)):
+    temp=li[i]+li[len(li)-1-i]
     if temp>MIN:
         MIN=temp
 
