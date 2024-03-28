5633cfbfd4298a1046635cf38f1c02eff874058d
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-28 15:52:15 +0900

    Title: 자연수 M/2개의 쌍, Time: 634ms, Memory: 29MB, Status: Passed - CodeTree

--- 240328/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
+++ 240328/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.py
@@ -1,6 +1,29 @@
-from sortedcontainers import SortedDict
 n=int(input())
-li=[list(map(int,input().split())) for _ in range(n)]
-li=sorted(li,key=lambda x:x[1])
+li=[]
+for _ in range(n):
+    x,y=map(int,input().split())
+    li.append([y,x])
+li.sort()
 
-print(li[0][1]+li[-1][1])
+l,r=0,n-1
+answer=0
+
+while True:
+    ly,ry=li[l],li[r]
+    if ly[1]==ry[1]:
+        answer=max(answer,ly[0]+ry[0])
+        l+=1
+        r-=1
+    else:
+        if ly[1]<ry[1]:
+            li[r][1]-=ly[1]
+            answer=max(answer,ly[0]+ry[0])
+            l+=1
+        else:
+            li[l][1]-=ry[1]
+            answer=max(answer,ly[0]+ry[0])
+            r-=1
+    if l>=r:
+        break
+
+print(answer)
