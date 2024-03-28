65e7f98e76dd4d2077ebdfc3c1f2f868ecc6fae8
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-28 16:14:12 +0900

    Title: 적석과 흑석 2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240328/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240328/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -6,15 +6,22 @@ red.sort()
 black.sort(key=lambda x:x[1]-x[0])
 
 ###################
-for b in black:
-    for r in red:
-        if b[0]<=r and b[1]>=r:
-            answer+=1
-            red.remove(r)
-            break
-        else:
-            red.remove(r)
-
+# for b in black:
+#     for r in red:
+#         if b[0]<=r and b[1]>=r:
+#             answer+=1
+#             red.remove(r)
+#             break
+cnt=-1
+for r in red:
+    cnt+=1
+    if cnt==n:
+        break
+    if black[cnt][0]<=r and black[cnt][1]>=r:
+        answer+=1
+    else:
+        continue
+    
 ###################
 
 print(answer)
