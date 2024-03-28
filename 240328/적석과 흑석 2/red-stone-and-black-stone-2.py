8267f87befd79020c266cd029f998362d5b9b809
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-03-28 16:41:12 +0900

    Title: 적석과 흑석 2, Time: 3829ms, Memory: 39MB, Status: Passed - CodeTree

--- 240328/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240328/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -1,20 +1,22 @@
 c,n=map(int,input().split())
 red=[int(input()) for _ in range(c)]
 black=[list(map(int,input().split())) for _ in range(n)]
+# c,n=6,8
+# red=[13,28,43,2,0,21] #[int(input()) for _ in range(c)]
+# black=[[0,21],[1,20],[12,14],[14,33],[22,38],[0,11],[12,19],[0,31]]
 answer=0
 red.sort()
+black.sort(key=lambda x:x[1]-x[0])
 black.sort(key=lambda x:x[1])
+# print(red)
+# print(black)
 ###################
 for b in black:
     for r in red:
-        if r>b[1]:
-            red.remove(r)
-            continue
         if b[0]<=r and b[1]>=r:
             answer+=1
             red.remove(r)
             break
-    
 # ###################
 
 print(answer)
