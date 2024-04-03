d5f792a87f5d6e27a9c2359ed5d8611f959da823
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-04-03 15:25:16 +0900

    Title: G & H 반전시키기, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240403/G & H 반전시키기/reversing-g-and-h.py
+++ 240403/G & H 반전시키기/reversing-g-and-h.py
@@ -2,30 +2,15 @@ n=int(input())
 init=input()
 final=input()
 
-def divide(base,strr):
-    li=[]
-    start=0
-    for i,n in enumerate(strr[1:]):
-        if i==len(strr)-2:
-            li.append((start,i+1))
-        if base!=n:
-            li.append((start,i))
-            start=i+1
-            base=n
-    return li
+answer=0
+mismatched = False
 
-li=divide(init[0],init)
-diff=[]
-cnt=0
-for n,(i,j) in enumerate(zip(init,final)):
-    if i!=j:
-        diff.append(n)
-for i in li:
-    ok=False
-    for j in diff:
-        if i[0]<=j and j<=i[1]:
-            diff.remove(j)
-            ok=True
-    if ok:
-        cnt+=1
-print(cnt)
+for i in range(n):
+    if init[i]!=final[i]:
+        if not mismatched:
+            mismatched=True
+            answer+=1
+
+    else:
+        mismatched = False
+    print(mismatched)
