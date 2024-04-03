2324705d825b769e3f53d0333ced0c23655526d6
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-04-03 18:55:59 +0900

    Title: 좌우 반전시키기, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240403/좌우 반전시키기/flip-left-and-right.py
+++ 240403/좌우 반전시키기/flip-left-and-right.py
@@ -5,30 +5,30 @@ ok=False
 def change(temp):
     return map(lambda x:1 if x==0 else 0,temp)
 
-# for _ in range(2):
-for i in range(1,n):
-    if i==n-1:
-        temp=li[i-1:i+1]
-        if temp[0]==0 and temp[1]==0:
-            cnt+=1
-            li[i-1:i+1]=change(temp)
-    else:
-        temp=li[i-1:i+2]
-        if i-1==0 and temp[0]==0:
-            cnt+=1
-            li[i-1:i+2]=change(temp)
+for _ in range(2):
+    for i in range(1,n):
+        if i==n-1:
+            temp=li[i-1:i+1]
+            if temp[0]==0 and temp[1]==0:
+                cnt+=1
+                li[i-1:i+1]=change(temp)
         else:
-            if temp[0]==0:
+            temp=li[i-1:i+2]
+            if i-1==0 and temp[0]==0:
                 cnt+=1
                 li[i-1:i+2]=change(temp)
-            elif temp[0]==1:
-                continue
             else:
-                cnt+=1
-                li[i-1:i+2]=change(temp)
-    if li==[1]*n:
-        ok=True
-        break
+                if temp[0]==0:
+                    cnt+=1
+                    li[i-1:i+2]=change(temp)
+                elif temp[0]==1:
+                    continue
+                else:
+                    cnt+=1
+                    li[i-1:i+2]=change(temp)
+        if li==[1]*n:
+            ok=True
+            break
 
 if ok:
     print(cnt)
