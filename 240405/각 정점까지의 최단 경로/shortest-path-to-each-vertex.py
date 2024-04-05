3e61249ca163432d90aae51f080786e408d51650
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-04-05 16:25:30 +0900

    Title: 각 정점까지의 최단 경로, Time: 1251ms, Memory: 90MB, Status: Passed - CodeTree

--- 240405/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
+++ 240405/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -2,12 +2,8 @@ import math,heapq
 
 n,m=map(int,input().split())
 k=int(input())
-
-graph=[[] for _ in range(n+1)]
-dist=[math.inf]*(n+1)
-pq=[]
-
 edges=[(-1,-1,-1)]
+graph=[[] for _ in range(n+1)]
 for _ in range(m):
     edges.append(tuple(map(int,input().split())))
 
@@ -16,20 +12,23 @@ for i in range(1,m+1):
     graph[x].append((y,z))
     graph[y].append((x,z))
 
+
+pq=[]
+dist=[math.inf]*(n+1)
+
 dist[k]=0
 heapq.heappush(pq,(0,k))
 
 while pq:
     min_dist,min_index=heapq.heappop(pq)
-
-    if min_dist!=dist[min_index]:
+    if dist[min_index]!=min_dist:
         continue
 
     for y,z in graph[min_index]:
         new_dist=dist[min_index]+z
-        if dist[y]>new_dist:
+        if new_dist<dist[y]:
             dist[y]=new_dist
-            heapq.heappush(pq,(new_dist,y))
+            heapq.heappush(pq, (new_dist,y))
 
 for i in range(1,n+1):
     if dist[i]==math.inf:
