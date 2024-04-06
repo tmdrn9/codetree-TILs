d9c2773a745d2e600b22ccb226a50c4a935d4055
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-04-06 16:13:37 +0900

    Title: 마을 구분하기, Time: 124ms, Memory: 25MB, Status: Passed - CodeTree

--- 240406/마을 구분하기/seperate-village.py
+++ 240406/마을 구분하기/seperate-village.py
@@ -1,7 +1,5 @@
 import heapq
 n=int(input())
-# grid=[list(map(int,input().split())) for _ in range(n)]
-# visited=[[False]*n for _ in range(n)]
 grid=[]
 visited=[]
 for _ in range(n):
@@ -9,31 +7,30 @@ for _ in range(n):
     grid.append(temp)
     visited.append(list(map(lambda x: False if x else True,temp)))
 
-
 people=[]
 
 def in_range(x,y):
     return 0<=x and x<n and 0<=y and y<n
-cnt=0
+
 def dfs(x,y):
     global cnt
     dx,dy=[0,1,0,-1],[1,0,-1,0]
     for i,j in zip(dx,dy):
         new_x,new_y=x+i,y+j
-        if in_range(new_x,new_y) and not visited[new_x][new_y] and grid[new_x][new_y]:
+        if in_range(new_x,new_y) and not visited[new_x][new_y]:
             visited[new_x][new_y]=True
-            cnt+=1
             dfs(new_x,new_y)
+            cnt+=1
 
 
 for i in range(n):
     for j in range(n):
-        cnt=0
         if visited[i][j]:
             continue
+        visited[i][j]=True
+        cnt=1
         dfs(i,j)
-        if cnt!=0:
-            heapq.heappush(people,cnt)
+        heapq.heappush(people,cnt)
 
 print(len(people))
 while people:
