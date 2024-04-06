93178d5a0c98941f79b3e9e0b142dc6753f9ecf6
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-04-06 21:39:40 +0900

    Title: 왕실의 기사 대결, Time: 156ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240406/왕실의 기사 대결/royal-knight-duel.py
@@ -0,0 +1,71 @@
+l,n,q=map(int,input().split())
+#1:함정, 2:벽
+grid=[list(map(int,input().split())) for _ in range(l)]
+
+# (r,c,h,w,k)/r,c:초기위치/hw세로가로/k체력
+people=[(-1,-1,-1,-1,-1)]
+for _ in range(n):
+    people.append(list(map(int,input().split())))
+
+people_grid=[[0]*l for _ in range(l)]
+
+for n,(r,c,h,w,k) in enumerate(people[1:]):
+    people[n+1][0]-=1
+    people[n+1][1]-=1
+    for i in range(r-1,r-1+h):
+        for j in range(c-1,c-1+w):
+            people_grid[i][j]=n+1
+
+#i:기사 번호, d:방향으로 1칸이동
+#d 0:위,1:오른,2:아래,3:왼
+king=[list(map(int,input().split())) for _ in range(q)]
+DX,DY=[-1,0,1,0],[0,1,0,-1]
+
+def in_range(x,y):
+    return 0<=x and x<l and 0<=y and y<l and grid[x][y]!=2
+
+def surround(n,d):
+    global other,ok
+    x,y,h,w,_=people[n]
+    for i in range(h):
+        for j in range(w):
+            new_x=x+i+DX[d]
+            new_y=y+j+DY[d]
+            if not in_range(new_x,new_y):
+                ok=True
+                return 
+            else:
+                if people_grid[new_x][new_y]==n or people_grid[new_x][new_y]==0:
+                    continue
+                other.add(people_grid[new_x][new_y])
+                surround(people_grid[new_x][new_y],d)
+def damage(i):
+    dmg=0
+    r,c,_,_,k=people[i]
+    if grid[r][c]==1:
+        people[i][4]-=1
+        dmg+=1
+    return dmg
+
+def move(i,d):
+    r,c,h,w,k=people[i]
+    for i in range(h):
+        for j in range(w):
+            people_grid[r+i][c+j]=0
+            people_grid[r+i+DX[d]][c+j+DY[d]]=i
+
+    people[i]=[r+DX[d],c+DY[d],h,w,k]
+
+answer=0
+for i,d in king:
+    other=set([i])
+    ok=False
+    surround(i,d)
+
+    if ok:
+        continue
+    for n in list(other)[::-1]:
+        move(n,d)
+        answer+=damage(n)
+
+print(answer)
