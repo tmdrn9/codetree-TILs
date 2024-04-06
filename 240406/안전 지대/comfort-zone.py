1d700a5abf8aa75bf7ece4ee85e57e27a90dab6d
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-04-06 17:38:02 +0900

    Title: 안전 지대, Time: 175ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240406/안전 지대/comfort-zone.py
+++ 240406/안전 지대/comfort-zone.py
@@ -1,4 +1,5 @@
 import heapq
+
 n,m=map(int,input().split())
 homes=[list(map(int,input().split())) for _ in range(n)]
 pq=[]
@@ -8,22 +9,26 @@ def in_range(x,y):
 def dfs(x,y):
     dx,dy=[0,1,0,-1],[1,0,-1,0]
     for i,j in zip(dx,dy):
-        new_x,new_y=x+i,y+j
+        new_x,new_y=x+i, y+j
         if in_range(new_x,new_y) and not visited[new_x][new_y]:
             visited[new_x][new_y]=True
             dfs(new_x,new_y)
+
 max_k= max(max(homes)) if len(homes)>1 else max(homes)
-for k in range(1,max_k-1):
-    visited=[]
-    for i in homes:
-        visited.append(list(map(lambda x: True if x<=k else False ,i)))
-    cnt=0
-    for i in range(n):
-        for j in range(m):
-            if visited[i][j]:
-                continue
-            dfs(i,j)
-            cnt+=1
-    heapq.heappush(pq,(-cnt,k))
-max_,k=heapq.heappop(pq)
-print(k,-max_)
+if max_k==1:
+    print(1,0)
+else:
+    for k in range(1,max_k-1):
+        visited=[]
+        for i in homes:
+            visited.append(list(map(lambda x: True if x<=k else False ,i)))
+        cnt=0
+        for i in range(n):
+            for j in range(m):
+                if visited[i][j]:
+                    continue
+                dfs(i,j)
+                cnt+=1
+        heapq.heappush(pq,(-cnt,k))
+    max_,k=heapq.heappop(pq)
+    print(k,-max_)
