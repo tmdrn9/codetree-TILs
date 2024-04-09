beecf9e8add657c7e7a806bde3a72b8423269f8e
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-04-10 00:48:36 +0900

    Title: 코드트리 빵, Time: 299ms, Memory: 27MB, Status: Passed - CodeTree

--- 240410/코드트리 빵/codetree-mon-bread.py
+++ 240410/코드트리 빵/codetree-mon-bread.py
@@ -4,39 +4,34 @@ DX,DY=[-1,0,0,1],[0,-1,1,0]
 
 def in_range(x,y):
     return 0<=x<N and 0<=y<N
-def bfs(x,y):
-    global grid
-    cp_grid=[[0]*N for _ in range(N)]
+
+def bfs(start_pos):
+    # visited, step 값을 전부 초기화합니다.
     for i in range(N):
         for j in range(N):
-            cp_grid[i][j]=grid[i][j]
-    q=deque([[x,y]])
-    cp_grid[x][y]=-1
+            visited[i][j] = False
+            step[i][j] = 255
+    
+    # 초기 위치를 넣어줍니다.
+    q = deque()
+    q.append(start_pos)
+    sx, sy = start_pos
+    visited[sx][sy] = True
+    step[sx][sy] = 0
+
+    # BFS를 진행합니다.
     while q:
-        x,y=q.popleft()
-        for dx, dy in zip(DX, DY):
-            nx,ny=x+dx,y+dy
-            if in_range(nx,ny) and cp_grid[nx][ny]!=-1:
-                if cp_grid[nx][ny]==1:
-                    return (nx,ny)
-                cp_grid[nx][ny]=-1
-                q.append([nx,ny])
-def bfs_(x,y):
-    global grid
-    cp_grid=[[0]*N for _ in range(N)]
-    for i in range(N):
-        for j in range(N):
-            cp_grid[i][j]=grid[i][j]
-    q=deque([[x,y]])
-    cp_grid[x][y]=-1
-    while q:
-        x,y=q.popleft()
-        for dx, dy in zip(DX, DY):
-            nx,ny=x+dx,y+dy
-            if in_range(nx,ny) and cp_grid[nx][ny]!=-1:
-                cp_grid[nx][ny]=-1
-                step[nx][ny]=step[x][y]+1
-                q.append([nx,ny])
+        # 가장 앞에 원소를 골라줍니다.
+        x, y = q.popleft()
+
+        # 인접한 칸을 보며 아직 방문하지 않은 칸을 큐에 넣어줍니다.
+        for dx, dy in zip(DX,DY):
+            nx, ny = x + dx, y + dy
+            # 갈 수 있는 경우에만 진행합니다.
+            if in_range(nx,ny) and visited[nx][ny]==False and grid[nx][ny]!=-1:
+                visited[nx][ny] = True
+                step[nx][ny] = step[x][y] + 1
+                q.append((nx, ny))
 
 N,M=map(int,input().split())
 stop=[0]*(M+1)
@@ -49,20 +44,20 @@ for i in range(1,M+1):
     arrive[i]=list(map(lambda x:int(x)-1,input().split()))
 t=0
 while len(set(stop))!=1:
+    step = [[255] * N for _ in range(N)]
+    visited = [[False] * N for _ in range(N)]
     # print(stop)
     t += 1
     del_=[]
     # 1 모두가 한칸씩 움직임 상좌우하 [-1,0,0,1],[0,-1,1,0]
     for i in range(1,min(t,M+1)):
-        step = [[255] * N for _ in range(N)]
-        visited = [[False] * N for _ in range(N)]
         if stop[i]==1:
             continue
         x,y=m[i]
         ax,ay=arrive[i]
         result=[]
         mm=math.inf
-        bfs_(ax,ay)
+        bfs((ax,ay))
         for dx,dy in zip(DX,DY):
             nx,ny=x+dx,y+dy
             if not in_range(nx,ny) or grid[nx][ny]==-1:
@@ -83,8 +78,15 @@ while len(set(stop))!=1:
     #3 만약 t<=m이면 t번 사람이 자기가 갈 편의점과 가장 가까이 있는 베캠으로 이동
     if t<M+1:
         ax, ay = arrive[t]
-        tx,ty=bfs(ax, ay)
-        m[t]=[tx,ty]
-        grid[tx][ty]=-1
+        bfs((ax, ay))
+        min_dist = math.inf
+        min_x, min_y = -1, -1
+        for i in range(N):
+            for j in range(N):
+                if visited[i][j] and grid[i][j] == 1 and min_dist > step[i][j]:
+                    min_dist = step[i][j]
+                    min_x, min_y = i, j
+        m[t]=[min_x,min_y]
+        grid[min_x][min_y]=-1
 
 print(t)
