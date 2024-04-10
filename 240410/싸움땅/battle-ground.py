9f95b38a8afcea3a51229d3b1d8db0d6c4a683f4
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-04-10 15:47:35 +0900

    Title: 싸움땅, Time: 330ms, Memory: 26MB, Status: Passed - CodeTree

--- 240410/싸움땅/battle-ground.py
+++ 240410/싸움땅/battle-ground.py
@@ -11,7 +11,20 @@ def move_player(i):
         new_d=(d+2)%4
         player_go[i]=new_d
         nx, ny = x+DX[new_d],y+DY[new_d]
-    player[i] = [nx, ny]
+    return nx,ny
+
+def move_loser(i):
+    x, y=player[i]
+    d=player_go[i]
+    nx,ny=x+DX[d],y+DY[d]
+    # 만약 이동하려는 칸에 다른 플레이어가 있거나 격자 범위 밖인 경우에는 오른쪽으로 90도씩 회전하여 빈 칸이 보이는 순간 이동합니다.
+    for ok in range(4):
+        if in_range(nx,ny) and not [nx,ny] in player:
+            player[i] = [nx, ny]
+            break
+        new_d=(player_go[i]+1)%4
+        player_go[i]=new_d
+        nx, ny = x+DX[new_d],y+DY[new_d]
     return nx,ny
 
 def fight(i,j,nx,ny):
@@ -31,20 +44,44 @@ def fight(i,j,nx,ny):
     ### 이긴플레이어:각 플레이어의 초기 능력치와 가지고 있는 총의 공격력의 합의 차이만큼을 포인트로 획득
     player_score[winner]+=score
     ### 진 플레이어: 본인이 가지고 있는 총을 해당 격자에 내려놓고, 해당 플레이어가 원래 가지고 있던 방향대로 한 칸 이동
-    grid[nx][ny]=player_gun[loser]
-    player_gun[loser]=0
-    move_player(loser)
+    ### 만약 이동하려는 칸에 다른 플레이어가 있거나 격자 범위 밖인 경우에는 오른쪽으로 90도씩 회전하여 빈 칸이 보이는 순간 이동합니다.
+    if player_gun[loser]!=0:
+        if grid[nx][ny]!=0:
+            grid[nx][ny].append(player_gun[loser])
+        else:
+            grid[nx][ny]=[player_gun[loser]]
+        player_gun[loser]=0
+
+    lx,ly=move_loser(loser)
+    if grid[lx][ly]!=0:
+        change_gun(loser, lx, ly)
     ### 이긴 플레이어: 승리한 칸에 떨어져 있는 총들과 원래 들고 있던 총 중 가장 공격력이 높은 총을 획득하고, 나머지 총들은 해당 격자에 내려 놓습니다.
-    change_gun(winner,nx,ny)
+    if grid[nx][ny]!=0:
+        change_gun(winner,nx,ny)
 
 
 def change_gun(i,nx,ny):
-    if player_gun[i] < grid[nx][ny]:
-        player_gun[i], grid[nx][ny] = grid[nx][ny], player_gun[i]
+    min_g=player_gun[i]
+    for g in grid[nx][ny]:
+        if min_g < g:
+            min_g = g
+    if min_g==player_gun[i]:
+        return
+    init=player_gun[i]
+    player_gun[i]=min_g
+    grid[nx][ny].remove(min_g)
+    if grid[nx][ny]==[] and init==0:
+        grid[nx][ny]=0
+    else:
+        grid[nx][ny].append(init)
 
 
 N, M, K = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if grid[i][j]!=0:
+            grid[i][j]=[grid[i][j]]
 player = [[-1, -1] for _ in range(M + 1)]  # 4는 총 공격력
 player_go = [-1] * (M + 1)
 player_init = [-1] * (M + 1)
@@ -62,11 +99,15 @@ for turn in range(1,K+1):
         nx,ny=move_player(i)
         ##총이 있는 경우, 해당 플레이어는 총을 획득합니다.
         ###플레이어가 이미 총을 가지고 있는 경우에는 놓여있는 총들과 플레이어가 가지고 있는 총 가운데 공격력이 더 쎈 총을 획득하고, 나머지 총들은 해당 격자에 둡니다.
-        if grid[nx][ny]>0:
-            #더 쎈 총을 획득하거나 그대로 있거나
-            change_gun(i,nx,ny)
-        elif [nx,ny] in player:
+        if [nx,ny] in player:
             hi=player.index([nx,ny])
+            player[i] = [nx, ny]
             fight(i,hi,nx,ny)
+        elif grid[nx][ny]!=0:
+            #더 쎈 총을 획득하거나 그대로 있거나
+            player[i] = [nx, ny]
+            change_gun(i,nx,ny)
+        else:
+            player[i] = [nx, ny]
 for i in range(1, M + 1):
     print(player_score[i],end=' ')
