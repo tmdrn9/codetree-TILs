a322a849202a29b093e8674a77cde52dd453bb3c
Seunglee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-04-12 00:12:54 +0900

    Title: 술래잡기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240412/술래잡기/hide-and-seek.py
@@ -0,0 +1,155 @@
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def distance(x1,y1,x2,y2):
+    return abs(x1-x2)+abs(y1-y2)
+
+def hello_runner(d):
+    global score
+    answer=[]
+    if d==0:#위
+        sx= 0 if x-3<0 else x-3
+        for i in range(sx,x+1):
+            if grid[i][y]==-1:
+                continue
+            if p_grid[i][y]>0:
+                score+=t*p_grid[i][y]
+                p_grid[i][y]=0
+                answer.append((i,y))
+    elif d==1: #오
+        sy = n-1 if y + 3 >= n else x + 3
+        for i in range(y, sy + 1):
+            if grid[x][i] == -1:
+                continue
+            if p_grid[x][i] > 0:
+                score += t * p_grid[x][i]
+                p_grid[x][i] = 0
+                answer.append((x,i))
+    elif d==2: #아
+        sx = n-1 if x + 3 >= n else x + 3
+        for i in range(x, sx + 1):
+            if grid[i][y] == -1:
+                continue
+            if p_grid[i][y] > 0:
+                score += t * p_grid[i][y]
+                p_grid[i][y] = 0
+                answer.append((i, y))
+    else: #왼
+        sy = 0 if y - 3 < 0 else y - 3
+        for i in range(sy, y + 1):
+            if grid[x][i] == -1:
+                continue
+            if p_grid[x][i] > 0:
+                score += t * p_grid[x][i]
+                p_grid[x][i] = 0
+                answer.append((x, i))
+    return answer
+
+
+
+MX,MY=[-1,0,1,0],[0,1,0,-1] #슐래 토네이도
+DX,DY=[0,0,1,-1],[-1,1,0,0] #좌오하상 #러너꺼
+
+score=0
+n, m, h, k=map(int, input().split())
+grid=[[0]*n for _ in range(n)]
+p_grid = [[0] * n for _ in range(n)]
+runner=[]
+runner_d=[]
+for _ in range(m):
+    x,y,d=map(int, input().split())
+    runner.append((x-1,y-1))
+    p_grid[x-1][y-1]+=1
+    runner_d.append(d)
+for _ in range(h):
+    x, y = map(int, input().split())
+    grid[x-1][y-1]=-1 #나무는 -1로 표시
+
+x,y=n//2,n//2 # 술래 위치
+dist = 1
+idx = 0
+inout=0
+t = 0
+total = 0
+t_dist = 0
+visited=[[False]*n for _ in range(n)]
+rm=[]
+# k번 반복
+for turn in range(1,k+1):
+
+    cp=[x[:] for x in p_grid]
+    for i in range(len(runner)):
+        # 1. 도망자 움직임 단, 거리가 3 이하인 도망자만 동시에 움직임
+        if i in rm:
+            continue
+        rx,ry=runner[i]
+        rd=runner_d[i]
+        if distance(x,y,rx,ry)<=3:
+            # 바라보고 있는 방향으로 한칸씩 이동
+            nx,ny= rx+DX[rd],ry+DY[rd]
+            # 움직이려는 칸에 술래가 있는 경우엔 움직이지 않음
+            if (nx,ny)==(x,y):
+                continue
+            # 격자를 벗어나는 경우 방향을 틀고, 술래가 없을때만 1칸 이동
+            if not in_range(nx,ny):
+                if rd==1:
+                    runner_d[i],rd=0,0
+                elif rd==0:
+                    runner_d[i],rd = 1,1
+                elif rd==2:
+                    runner_d[i],rd = 3,3
+                else:
+                    runner_d[i],rd = 2,2
+                nx,ny= rx+DX[rd],ry+DY[rd]
+                if (nx, ny) == (x, y):
+                    continue
+                else:
+                    cp[rx][ry] -= 1
+                    cp[nx][ny] += 1
+                    runner[i]=(nx,ny)
+            else:
+                cp[rx][ry] -= 1
+                cp[nx][ny] += 1
+                runner[i] = (nx, ny)
+
+        else:
+            continue
+    p_grid=cp
+
+    #t_dist,dist,idx,inout,t,total
+    if inout==0:
+        x, y = x + MX[idx], y + MY[idx]
+        t+=1
+        t_dist+=1
+        if (x, y) == (0, 0):
+            inout=1
+            total,dist,t,idx=0,0,0,2
+        else:
+            if t_dist==dist:
+                idx = (idx + 1) % 4
+                t_dist=0
+            if t==total+dist*2:
+                total+=t
+                dist+=1
+    #밖에서 안으로
+    else:
+        x, y = x + MX[idx], y + MX[idx]
+        if (x, y) == (n//2, n//2):
+            visited = [[False] * n for _ in range(n)]
+            visited[0][0]=True
+            idx=0
+            inout=0
+        else:
+            if visited[x + MX[idx]][y + MY[idx]] or in_range(x + MX[idx], y + MY[idx])==False:
+                idx=(idx-1)%4
+
+    rm_li=hello_runner(idx)
+    for i in range(len(runner)):
+        if runner[i] in rm_li:
+            rm.append(i)
+print(score)
+
+    ## 토네이도로 한칸 움직임
+    ### 바라보고 있는 방향기준으로 현재칸 포함해 3칸 내에 있는 도망자 잡기
+    ### 나무와 같이 있는 도망자는 안잡힘
+    ### 도망자 잡으면 현재 턴t*잡은도망자수만큼 점수획득
