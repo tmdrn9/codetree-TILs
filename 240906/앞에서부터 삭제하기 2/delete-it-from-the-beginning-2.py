fbe7c89cd89e33772434835e3e1abbb3953cc715
Seung-Lee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-09-06 16:07:55 +0900

    Title: 앞에서부터 삭제하기 2, Time: 159ms, Memory: 32MB, Status: Passed - CodeTree

--- 240906/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
+++ 240906/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.py
@@ -2,11 +2,16 @@ import heapq
 
 n=int(input())
 li=list(map(int, input().split()))
+pq=[li[-1]]
+summ=li[-1]
 
-mean_li=[]
-for k in range(1,n-1):
-    temp=li[k:]
-    heapq.heapify(temp)
-    heapq.heappop(temp)
-    mean_li.append(sum(temp)/(n-k-1))
-print(f"{max(mean_li):.2f}")
+max_avg=0
+for k in range(n-2,0,-1):
+    heapq.heappush(pq,li[k])
+    summ+=li[k]
+
+    min_n=pq[0]
+    temp_avg=((summ-min_n)/(n-k-1))
+    if temp_avg>max_avg:
+        max_avg=temp_avg
+print(f"{max_avg:.2f}")
