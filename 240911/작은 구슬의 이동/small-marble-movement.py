a843e67677600e4e92f10754eae99a11721716b9
Seung-Lee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-09-11 16:18:06 +0900

    Title: 작은 구슬의 이동, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240911/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,24 @@
+dx,dy=[1,0,-1,0],[0,1,0,-1]
+di = {}  # dict 초기화
+di['R'] = 0
+di['D'] = 1
+di['U'] = 3
+di['L'] = 2
+
+n,t = map(int,input().split())
+
+def in_range(x,y):
+    return 1<=x and x<n+1 and 1<=y and y<n+1
+
+r, c, d=input().split()
+d=di[d]
+r=int(r)
+c=int(c)
+
+for _ in range(t):
+    tx,ty=r+dy[d],c+dx[d]
+    if in_range(tx,ty):
+        r,c=tx,ty
+    else:
+        d=2-d
+print(r,c)
