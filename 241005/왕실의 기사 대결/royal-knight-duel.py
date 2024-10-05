8424865b828675ec7aacf7e115650c7bf8e90953
Seung-Lee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2024-10-05 23:42:51 +0900

    Title: 왕실의 기사 대결, Time: 224ms, Memory: 25MB, Status: Passed - CodeTree

--- 241005/왕실의 기사 대결/royal-knight-duel.py
+++ 241005/왕실의 기사 대결/royal-knight-duel.py
@@ -1,15 +1,15 @@
 from collections import deque
 
 dxs,dys=[-1,0,1,0],[0,1,0,-1]
-def in_range(x,y):
-    1<=x and x<=L and 1<=y and y<=L and grid[x][y]==2
+
 
 L, N, Q=map(int,input().split())
-grid=[[2]*(L+2)]+[[2]+ list(map(int,input().split())) for _ in range(L)+[2]]+[[2]*(L+2)]
+grid=[[2]*(L+2)]+[[2]+ list(map(int,input().split()))+[2] for _ in range(L) ]+[[2]*(L+2)]
 p={}
 init_k=[0]*(N+1)
 for i in range(1,N+1):
     p[i]=list(map(int,input().split()))
+    init_k[i]=p[i][4]
 
 def fight(start,d):
     q=deque()
@@ -19,29 +19,29 @@ def fight(start,d):
     q_set.add(start)
     while q:
         cur=q.popleft()
-        p[cur]=si,sj,h,w,k
+        si,sj,h,w,k=p[cur]
         ni,nj=si+dxs[d],sj+dys[d]
         for i in range(ni,ni+h):
             for j in range(nj,nj+w):
-                if not in_range(i,j):
+                if grid[i][j]==2:#not in_range(i,j):
                     return
                 if grid[i][j]==1:
                     damage[cur]+=1
         for idx in p:
             if idx in q_set:
                 continue
-            p[idx]=ti,tj,th,tw,tk
+            ti,tj,th,tw,tk=p[idx]
             if ni<=ti+th-1 and ti<=ni+h-1 and nj<=tj+tw-1 and tj<=nj+w-1:
                 q.append(idx)
                 q_set.add(idx)
 
     damage[start]=0
     for idx in q_set:
-        p[idx]=ci,cj,h,w,k
+        ci,cj,h,w,k=p[idx]
         if k<=damage[idx]:
             p.pop(idx)
         else:
-            p[idx]=[ci+dxs[d],cj+dys[d],h,w,k-damage[d]]
+            p[idx]=[ci+dxs[d],cj+dys[d],h,w,k-damage[idx]]
                 
             
                 
