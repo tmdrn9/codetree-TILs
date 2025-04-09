605968b06cddf074dc09fe3db08a5cdf7f260180
Seung-Lee Lee/이승리 <77779116+tmdrn9@users.noreply.github.com>
2025-04-09 14:37:18 +0900

    Title: 메이즈 러너, Time: 92ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250409/메이즈 러너/maze-runner.py
+++ 250409/메이즈 러너/maze-runner.py
@@ -1,14 +1,10 @@
 #시작 1:00
 #설계 끝 1:14
-#코딩 끝 2:24
-
-# import sys
-# sys.stdin=open('input.txt')
-# input=sys.stdin.readline
+#코딩 끝 2:37
+#people array제거
 
 import heapq
 N,M,K=map(int,input().split())
-people=[]
 grid=[list(map(int,input().split())) for _ in range(N)]
 
 for _ in range(M):
@@ -16,7 +12,6 @@ for _ in range(M):
     r-=1
     c-=1
     grid[r][c]-=1
-    people.append((r,c))
 exit_r, exit_c = map(int,input().split())
 exit_r-=1
 exit_c-=1
@@ -31,58 +26,54 @@ def in_range(x,y):
     return 0<=x<N and 0<=y<N
 
 def move_people():
-    global move_count,grid, people
+    global move_count,grid
     dxs,dys=[-1,1,0,0],[0,0,-1,1]
     ## 움직이는칸은 현재 머물러있던 칸보다 출구까지의 거리가 가까워야함
     ## 움직일 수 없으면 움직이지 않음
     ## 한 칸에 두명 이상의 참가자가 있을 수 있음
     ## 참가자가 출구에 도달하면 즉시 탈출
-    temp_people=[]
     temp=[row[:] for row in grid]
-    for r,c in people:
-        move=False
-        n_people=grid[r][c]
-        if distance(exit_r, exit_c,r,c)==1:
-            move = True
-            move_count -= n_people
-            temp[r][c] -= n_people
-        else:
-            for dx,dy in zip(dxs,dys):
-                nr,nc=r+dx,c+dy
-                if (0<=nr<N and 0<=nc<N and grid[nr][nc]<=0) and distance(nr,nc,exit_r, exit_c)<distance(r,c,exit_r, exit_c):
-                    move=True
-                    move_count-=n_people
-                    temp[r][c]-=n_people
-                    temp[nr][nc]+=n_people
-                    temp_people.append((nr, nc))
-                    break
-        if not move:
-            temp_people.append((r,c))
-    people=temp_people
+    for r in range(N):
+        for c in range(N):
+            if grid[r][c] < 0:
+                n_people=grid[r][c]
+                if distance(exit_r, exit_c,r,c)==1:
+                    move_count -= n_people
+                    temp[r][c] -= n_people
+                else:
+                    for dx,dy in zip(dxs,dys):
+                        nr,nc=r+dx,c+dy
+                        if (0<=nr<N and 0<=nc<N and grid[nr][nc]<=0) and distance(nr,nc,exit_r, exit_c)<distance(r,c,exit_r, exit_c):
+                            move_count-=n_people
+                            temp[r][c]-=n_people
+                            temp[nr][nc]+=n_people
+                            break
     grid=temp
 
 def find_box():
     ### 2개 이상이면 r작은것>c작은것
     q=[]
-    for r,c in people:
-        rr=abs(r-exit_r)
-        cc=abs(c-exit_c)
-        box_size=max(rr,cc)
-        if box_size==rr:
-            br= r if r<exit_r else exit_r
-            if c<=exit_c:
-                bc=0 if exit_c-box_size<=0 else exit_c-box_size
-            else:
-                bc =0 if c-box_size<=0 else c-box_size
+    for r in range(N):
+        for c in range(N):
+            if grid[r][c]<0:
+                rr=abs(r-exit_r)
+                cc=abs(c-exit_c)
+                box_size=max(rr,cc)
+                if box_size==rr:
+                    br= r if r<exit_r else exit_r
+                    if c<=exit_c:
+                        bc=0 if exit_c-box_size<=0 else exit_c-box_size
+                    else:
+                        bc =0 if c-box_size<=0 else c-box_size
 
-        else:
-            bc= c if c<exit_c else exit_c
-            if r<=exit_r:
-                br =0 if exit_r-box_size<=0 else exit_r-box_size
-            else:
-                br = 0 if r - box_size <= 0 else r - box_size
+                else:
+                    bc= c if c<exit_c else exit_c
+                    if r<=exit_r:
+                        br =0 if exit_r-box_size<=0 else exit_r-box_size
+                    else:
+                        br = 0 if r - box_size <= 0 else r - box_size
 
-        heapq.heappush(q,(box_size,br,bc))
+                heapq.heappush(q,(box_size,br,bc))
 
     return heapq.heappop(q)
 
@@ -98,17 +89,12 @@ def rotate90(size,r,c):
                 result[r + j][c + size - i - 1] = grid[r + i][c + j]
                 if grid[r + i][c + j] == 10:
                     exit_r, exit_c =r + j,c + size - i - 1
-                elif grid[r + i][c + j] <0:
-                    people.remove((r+i,c+j))
-                    people.append((r+j,c+size-i-1))
     grid=result
 
 for turn in range(K):
     #1. 모든 참가지는 동시에 한칸씩 움직임 =>함수
     move_people()
 
-    if len(people)<=0: #모든 참가자 탈출
-        break
 
     #2. 미로회전
     ##한명 이상의 참가자와 출구를 포함한 가장작은 정사각형 선택 =>함수화
